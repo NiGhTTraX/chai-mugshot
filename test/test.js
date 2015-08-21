b9ba09b16befc08ac7974ec5fb96c0613f3b2d98
Simion Florentin <florentin.simion@hootsuite.com>
2015-08-21 18:03:24 +0300

    add integration tests

---
+++ test/test.js
@@ -0,0 +1,73 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+var wdio = require('webdriverio');
+var wdioAdaptor = require('../mugshot/lib/adaptors/webdriverio.js');
+var Mugshot = require('../mugshot/lib/mugshot.js');
+var chaiMugshot = require('../index.js');
+var fs = require('fs');
+var path = require('path');
+
+var name = 'great';
+
+function cleanUp() {
+  var ext = '.png';
+  var dir  = path.join(__dirname, '../visual-tests');
+
+  fs.unlink(path.join(dir, name + ext), function() {});
+  fs.unlink(path.join(dir, name + '.new' + ext), function() {});
+  fs.unlink(path.join(dir, name + '.diff' + ext), function() {});
+
+  fs.rmdirSync(dir);
+}
+
+describe('Chai-Mugshot Plugin', function() {
+  this.timeout(0);
+
+  var url = 'file:///Users/florentin-simion/Desktop/chai-mugshot/test/test.htm',
+      noSelector = {
+        name: name
+      },
+      withSelector  = {
+        name: name,
+        selector: '#rectangle'
+      },
+      wdioInstance;
+
+  before(function(done) {
+    var options = {
+      desiredCapabilities: {
+        browserName: 'firefox'
+      }
+    };
+
+    wdioInstance = wdio.remote(options).init().url(url)
+      .then(function() {
+        var browser = new wdioAdaptor(wdioInstance);
+
+        var mugshot = new Mugshot(browser);
+
+        chai.use(chaiMugshot(mugshot));
+        done();
+      });
+  });
+
+  it('should be identical if there is no previous baseline', function() {
+
+    return expect(withSelector).to.be.identical;
+  });
+
+  it('should be identical if there are no differences', function() {
+
+    return expect(withSelector).to.be.identical;
+  });
+
+  it('should not be identical if there are differences', function() {
+
+    return expect(noSelector).to.not.be.identical;
+  });
+
+  after(function() {
+    cleanUp();
+    return wdioInstance.end();
+  });
+});
