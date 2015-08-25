b5051ccc6fc22a2d4149e3b8e7d5dac6fc126bb0
Simion Florentin <florentin.simion@hootsuite.com>
2015-08-25 18:34:46 +0300

    refactoring tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,15 @@
 var chai = require('chai');
 var expect = require('chai').expect;
-var Mugshot = require('mugshot');
+var AssertionError = require('chai').AssertionError;
+var chaiAsPromised = require('chai-as-promised');
 var chaiMugshot = require('../index.js');
+var Mugshot = require('mugshot');
 var wdioAdapter = Mugshot.adapters.WebdriverIO;
 var wdio = require('webdriverio');
 var fs = require('fs');
 var path = require('path');
+
+chai.use(chaiAsPromised);
 
 var name = 'great';
 
@@ -17,19 +21,26 @@ function cleanUp() {
   fs.unlink(path.join(dir, name + '.new' + ext), function() {});
   fs.unlink(path.join(dir, name + '.diff' + ext), function() {});
 
-  fs.rmdirSync(dir);
+  fs.rmdir(dir, function() {});
 }
 
 describe('Chai-Mugshot Plugin', function() {
   this.timeout(0);
 
   var url = 'file://' + path.join(__dirname, 'test.html'),
+      brokenSelector = {
+        name: name,
+        selector: 'anything'
+      },
       noSelector = {
         name: name
       },
       withSelector  = {
         name: name,
-        selector: '#rectangle'
+        selector: '#greenRectangle'
+      },
+      fulfilledThrowingProm = {
+        state: 'pending'
       },
       wdioInstance;
 
@@ -49,16 +60,28 @@ describe('Chai-Mugshot Plugin', function() {
       });
   });
 
-  it('should be identical if there is no previous baseline', function() {
-    return expect(withSelector).to.be.identical;
+  it('should be rejected if mugshot fails', function() {
+    return expect(expect(brokenSelector).to.be.identical).to.be.rejected;
   });
 
-  it('should be identical if there are no differences', function() {
-    return expect(withSelector).to.be.identical;
+  it('should not throw if there is no previous baseline', function() {
+    return expect(expect(withSelector).to.be.identical).to.be.fulfilled;
   });
 
-  it('should not be identical if there are differences', function() {
-    return expect(noSelector).to.not.be.identical;
+  it('should not throw if there is expected to be equal', function() {
+    return expect(expect(withSelector).to.be.identical).to.be.fulfilled;
+  });
+
+  it('should throw error if there is expected to not be equal', function() {
+    return expect(expect(withSelector).to.not.be.identical).to.be.fulfilled;
+  });
+
+  it('should not throw if there is expected to have differences', function() {
+    return expect(expect(noSelector).to.not.be.identical).to.be.fulfilled;
+  });
+
+  it('should throw if there is not expected to have differences', function() {
+    return expect(expect(noSelector).to.be.identical).to.be.fulfilled;
   });
 
   after(function() {
